4f150ee1332a6e9b76bec19ccf76e5bbae9918f7
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-25 20:53:12 -0500

    initial commit

---
+++ blog/src/App.jsx
@@ -0,0 +1,14 @@
+import Posts from "./components/posts";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello world!</h1>
+        <Posts />
+      </div>
+    </>
+  );
+};
+
+export default App;

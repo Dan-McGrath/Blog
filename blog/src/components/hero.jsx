5fa6d704d8591e43f225561dea4dd8be6a72a9d1
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-25 20:56:50 -0500

    created basic hero component

---
+++ blog/src/components/hero.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+  return (
+    <div className="hero">
+      <h1>Welcome To Dan&apos;s World</h1>
+      <p>This is a blog I created using the MERN stack.</p>
+    </div>
+  );
+};
+
+export default Hero;

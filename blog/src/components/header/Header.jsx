65491347556565aa795385d7b60d7eb6df93fdb0
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 16:45:48 -0500

    create basic header

---
+++ blog/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="logo">
+          <img src="" alt="logo" />
+        </div>
+        <Nav />
+      </header>
+    </>
+  );
+};
+
+export default Header;

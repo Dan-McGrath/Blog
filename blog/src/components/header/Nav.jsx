c96379da1b5fb26a5fbb287263c1716c8a4f42a9
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 16:45:54 -0500

    create basic nav

---
+++ blog/src/components/header/Nav.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <Link to="/">Home</Link>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;

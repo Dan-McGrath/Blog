96ce8d057d47a09c94fc1b7966e2b4c6962f55a4
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 17:27:48 -0500

    move loader to home page

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,15 +1,7 @@
-import { Outlet, useLoaderData } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { fetchPosts } from "./helpers/api";
+import { Outlet } from "react-router-dom";
 import Header from "./components/header/Header";
 
-export const loader = async () => {
-  const posts = await fetchPosts();
-  return { posts };
-};
-
 const App = () => {
-  const posts = useLoaderData();
   return (
     <>
       <Header />

9106657ab83f87e11000f51325f55224569fd1d0
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 22:08:43 -0500

    set up style components

---
+++ blog/src/components/posts/postDetailStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Title = styled.h2``;
+
+const Author = styled.p``;
+
+const CommentsWrapper = styled.div``;
+
+const Article = styled.article``;
+
+export { Wrapper, Title, Author, CommentsWrapper, Article };

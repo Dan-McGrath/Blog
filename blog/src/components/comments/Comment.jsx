35ecda32c0b3e0459b8736870e8ac183a75598b4
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 21:24:05 -0500

    display comment info map replies to replies component

--- blog/src/components/comments/Comment.jsx
+++ blog/src/components/comments/Comment.jsx
@@ -1,3 +1,20 @@
-const Comments = () => {};
+import PropTypes from "prop-types";
+import Replies from "../Replies/Replies";
 
-export default Comments;
+const Comment = ({ comment }) => {
+  return (
+    <>
+      <div>
+        <p>{comment.text}</p>
+        <p>{comment.name}</p>
+        <Replies replies={comment.replies} />
+      </div>
+    </>
+  );
+};
+
+Comment.propTypes = {
+  comment: PropTypes.object,
+};
+
+export default Comment;

e5aa55516a7b2496c7da9bf060d994e62bb10a18
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 22:09:00 -0500

    set up style components

---
+++ blog/src/components/router/errorPageStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Title = styled.h3``;
+
+const ErrorWrapper = styled.div``;
+
+const ErrorText = styled.p``;
+
+export { Wrapper, Title, ErrorText, ErrorWrapper };

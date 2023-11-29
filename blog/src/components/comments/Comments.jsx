e6e3c7487423897c5baec119655674ad0148af3c
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 21:23:34 -0500

    map comments to comment component

--- blog/src/components/comments/Comments.jsx
+++ blog/src/components/comments/Comments.jsx
@@ -1,3 +1,28 @@
-const Comments = () => {};
+import Comment from "./comment";
+import PropTypes from "prop-types";
+const Comments = ({ comments }) => {
+  comments.forEach((ele) => {
+    if (ele.replies) {
+      ele.replies.forEach((reply) => {
+        let index = comments.findIndex((ele) => ele._id === reply._id);
+        if (index === -1) {
+          return;
+        }
+        comments.splice(index, 1);
+      });
+    }
+  });
+  return (
+    <>
+      {comments.map((comment) => (
+        <Comment key={comment._id} comment={comment} />
+      ))}
+    </>
+  );
+};
+
+Comments.propTypes = {
+  comments: PropTypes.array,
+};
 
 export default Comments;

16070ade2020b1c543b5f992ee3fda6d3a6571cc
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 22:09:30 -0500

    add styled components

--- blog/src/components/posts/Posts.jsx
+++ blog/src/components/posts/Posts.jsx
@@ -1,14 +1,11 @@
 import Post from "./post";
 import PropTypes from "prop-types";
+import { Wrapper } from "./postsStyles";
 
 const Posts = ({ postArray }) => {
-  return (
-    <>
-      <div className="posts">
-        <Post postArray={postArray} />
-      </div>
-    </>
-  );
+  <Wrapper>
+    <Post postArray={postArray} />
+  </Wrapper>;
 };
 
 Posts.propTypes = {

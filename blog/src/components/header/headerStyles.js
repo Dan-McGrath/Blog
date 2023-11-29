906468dc2e7366bbfc1e4b701a2f6ea8f8166899
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 22:08:37 -0500

    set up style components

---
+++ blog/src/components/header/headerStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.header``;
+
+const ImgWrapper = styled.div``;
+
+const Img = styled.img``;
+
+const NavWrapper = styled.div``;
+
+export { HeaderWrapper, ImgWrapper, Img, NavWrapper };

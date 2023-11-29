1862d5890e8ffc4ed475d6726524da11f97c5035
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-28 22:08:34 -0500

    set up style components

---
+++ blog/src/components/comments/commentStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const Text = styled.div``;
+
+const Author = styled.div``;
+
+const RepliesWrapper = styled.div``;
+
+export { Wrapper, Text, Author, RepliesWrapper };
